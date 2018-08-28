e60389cb04b0fe6e542bbbf8f48a586b8d98927f
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2018-08-28 16:14:26 +0200

    Close #32 - OnlyPediatricPatients

--- R/OnlyPediatricPatients.R
+++ R/OnlyPediatricPatients.R
@@ -1,3 +1,15 @@
+#' Only pediatric patients
+#'
+#' Keeps only the pediatric patients in the sample.
+#' @param sample Data frame. The study sample. No default.
+#' @param age.variable.name Character vector of length 1. The name of the age
+#'     variable. Defaults to "age".
+#' @param age.cutoff Numeric vector of length 1. The age cutoff. Defaults to 18,
+#'     i.e. only observations with an age less than 18 are kept in the sample.
+#' @param remove.missing Logical vector of length 1. If TRUE all observations
+#'     with missing age, as detected by is.na, are removed from the
+#'     sample. Defaults to TRUE.
+#' @export
 OnlyPediatricPatients <- function(sample, age.variable.name = "age", age.cutoff = 18, remove.missing = TRUE) {
     ## Error handling
     if (!is.data.frame(study.data))
@@ -21,9 +33,16 @@ OnlyPediatricPatients <- function(sample, age.variable.name = "age", age.cutoff 
     subsample <- subsample[subsample[, age.variable.name] < age.cutoff, ]
     n.excluded <- nrow(sample) - nrow(subsample) - n.missing
     ## Collate return list
-    return.list <- list(exclusion.text = paste0(n.missing + n.excluded, " excluded: \n",
-                                                "- ", n.missing, " had missing age \n",
-                                                "- ", n.excluded, " were adults"),
+    total.n.excluded <- n.excluded
+    if (remove.missing)
+        total.n.excluded <- total.n.excluded + n.missing
+    exclusion.text <- paste0(total.n.excluded, " were adults.")
+    if (remove.missing) {
+        exclusion.text <- paste0(total.n.excluded, " excluded: \n",
+                                 "- ", n.missing, " had missing age \n",
+                                 "- ", n.excluded, " were adults (aged 18 years or above)")
+    }
+    return.list <- list(exclusion.text = exclusion.text,
                         subsample = subsample)
     return(return.list)
 }
