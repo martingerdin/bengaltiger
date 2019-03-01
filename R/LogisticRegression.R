9c10124c4fca15b55a27f19cf4db2191ee950f54
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-03-02 00:10:55 +0100

    Fix #91

--- R/LogisticRegression.R
+++ R/LogisticRegression.R
@@ -6,6 +6,9 @@
 #'     variable. No default.
 #' @param covariate.names Character vector. The names of the covariates. No
 #'     default.
+#' @param run.bivariable.analyses Logical vector of length 1. If TRUE bivariable
+#'     analyses are run before the full model is run and these results are
+#'     included in the final table as unadjusted estimates. Defaults to FALSE.
 #' @param stop.if.missing Logical vector of length 1. If TRUE the execution
 #'     stops if there is missing data in the outcome or covariates. Defaults to
 #'     TRUE.
@@ -21,6 +24,7 @@
 #'     function runs. Useful for debugging. Defaults to FALSE.
 #' @export
 LogisticRegression <- function(study.sample, outcome.name, covariate.names,
+                               run.bivariable.analyses = FALSE,
                                stop.if.missing = TRUE, create.table = TRUE,
                                table.options = list(odds.ratio = TRUE,
                                                     confidence.interval = 0.95,
@@ -37,6 +41,8 @@ LogisticRegression <- function(study.sample, outcome.name, covariate.names,
         stop("outcome.name has to be a character vector of length 1")
     if (!is.character(covariate.names))
         stop("covariate.names has to be a character vector")
+    if (!is.logical(run.bivariable.analyses) | !IsLength1(run.bivariable.analyses))
+        stop("run.bivariable.analyses has to be a logical vector of length 1")    
     if (!is.logical(stop.if.missing) | !IsLength1(stop.if.missing))
         stop("stop.if.missing has to be a logical vector of length 1")
     if (!is.logical(create.table) | !IsLength1(create.table))
@@ -61,15 +67,24 @@ LogisticRegression <- function(study.sample, outcome.name, covariate.names,
                     "the analysis with missing data and let R use its default \n",
                     "setting to remove this data please set stop.if.missing to \n",
                     "FALSE and run again. \n\n")))
-    ## Build model
+    ## Run bivariable analyses
+    bivariable.model <- list()
+    if (run.bivariable.analyses) {
+        bivariable.models <- lapply(covariate.names, function(covariate.name) {
+            model <- glm(as.formula(paste0(outcome.name, " ~ ", covariate.name)), data = study.sample, family = binomial)
+            return(model)
+        })
+    }
+    ## Build full model
     model <- glm(model.outcome ~ ., data = model.data, family = binomial)
-    return.object <- model
-    ## Create table        
+    ## Create model list
+    return.object <- model.list <- c(list(model), bivariable.models)
+        ## Create table        
     if (create.table) {
-        table.options$model <- model
+        table.options$model.list <- model.list
         model.table <- do.call(CreateLogisticRegressionTable, table.options)
         return.object <- model.table
     }
-    ## Return
+   ## Return
     return(return.object)
 }
